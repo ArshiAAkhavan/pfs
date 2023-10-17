f5e5a60fd04ba9c5a1207ec1328135bf09c75fac
Ark <ark@sotoon.ir>
2023-10-17 10:58:36 +0330

    never fail in beegfs-cleanup

--- 1.fio-benchmark/beegfs-cleanup.sh
+++ 1.fio-benchmark/beegfs-cleanup.sh
@@ -5,4 +5,4 @@ DISK_MOUNT_PATH=/mnt/beegfs-server
 docker stop `docker ps -a --format '{{.Names}}' | grep beegfs`
 docker rm `docker ps -a --format '{{.Names}}' | grep beegfs`
 docker volume rm `docker volume ls -a --format '{{.Name}}' | grep beegfs`
-umount $DISK_MOUNT_PATH
+umount $DISK_MOUNT_PATH || true
